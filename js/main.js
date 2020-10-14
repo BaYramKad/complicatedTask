9f53da6b959cecc8ec542a06bd66ed6eb3241416
Bayram Emiraliev <bayram.kad@gmail.com>
2020-10-14 17:35:58 +0300

    Выполнил задание lesson03

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("abby");
+let money = "Hello, i FrontEnd Developer";
+let income = 70000 + " " + "руб";
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert (money);
+console.log(income);

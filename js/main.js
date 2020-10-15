f937b7f3efc9eddc6f5127bdb2913e86368dc21e
Bayram Emiraliev <bayram.kad@gmail.com>
2020-10-15 23:26:57 +0300

    Выполнил третее задание

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-let money = "Hello, i FrontEnd Developer";
-let income = 70000 + " " + "руб";
-let addExpenses;
-let deposit;
-let mission;
-let period;
 
-alert (money);
-console.log(income);
+let money = 30000 + " руб";
+let income = 10000;
+let addExpenses = "Комуналка 2 000 руб";
+let deposit = true;
+let mission = "4 500 000 руб";
+let period = 10 + " месяцев";
+let budgetDay = "30 000 руб";
+
+console.log(budgetDay);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`"Период равен = ${period}" и "Цель заработать ${mission}"`);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(" "));
